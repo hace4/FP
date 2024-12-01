7f829e3b48f9e922df16993d0bdf001e29428856
Hace4 <shmelkov03012006@gmail.com>
2024-12-01 12:56:54 +0300

    Ашч

--- search.php
+++ search.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 if (isset($_GET['assorty_id'])) {
     $assorty_id = $_GET['assorty_id'];
-    $search = isset($_GET['search']) ? "%" . $_GET['search'] . "%" : "%";
+    $search = isset($_GET['search']) ? $_GET['search'] . "%" : "%";
 
-    // Если search пуст, запрос вернет все товары из ассортимента
+    // Поиск начинается с первой буквы
     $query = $db->prepare("SELECT * FROM tovar WHERE name LIKE ? AND assorty_id = ?");
     $query->execute([$search, $assorty_id]);
     echo json_encode($query->fetchAll(PDO::FETCH_ASSOC));
